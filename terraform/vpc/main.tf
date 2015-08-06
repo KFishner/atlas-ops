45a1a8b99154f98a4488c383e73e6c6a3374b05c
Kevin Fishner <KFishner@gmail.com>
2015-08-05 23:56:01 -0700

    update route table assoc

--- terraform/vpc/main.tf
+++ terraform/vpc/main.tf
@@ -21,7 +21,7 @@ resource "aws_route_table" "r" {
     }
 }
 
-resource "aws_main_route_table_association" "a" {
-    vpc_id = "${aws_vpc.main.id}"
+resource "aws_route_table_association" "a" {
+    subnet_id = "${aws_subnet.main.id}"
     route_table_id = "${aws_route_table.r.id}"
 }
