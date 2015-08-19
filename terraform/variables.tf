0d0a6f3daaea12edb9f40dff1b30cff2b01ccaa4
Kevin Fishner <KFishner@gmail.com>
2015-08-18 21:36:38 -0700

    repro case

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -19,3 +19,15 @@ variable "consul_server_count" {
 }
 
 variable "nodejs_count" { }
+
+variable "sec_groups" {
+    default = {
+      dev = "sg-5781c432"
+      production = "sg-0cf28f69,sg-8d4439e8"
+    }
+}
+
+variable "env" {
+    description = "Environment in which to setup new cluster"
+    default = "dev"
+}
