cfaaeca6f2fc74aa2ee7cfd6c9de0210e96aa33d
Kevin Fishner <KFishner@gmail.com>
2015-08-25 21:43:45 -0700

    cyclessss

--- terraform/vpc/main.tf
+++ terraform/vpc/main.tf
@@ -1,6 +1,10 @@
 resource "aws_vpc" "main" {
     cidr_block = "${var.vpc-cidr}"
     enable_dns_hostnames = true
+
+    lifecycle = {
+      create_before_destroy = true
+    }
 }
 
 resource "aws_subnet" "main" {
@@ -8,10 +12,18 @@ resource "aws_subnet" "main" {
     cidr_block = "${var.subnet-cidr}"
     map_public_ip_on_launch = true
     availability_zone = "${var.azs}"
+
+    lifecycle = {
+      create_before_destroy = true
+    }
 }
 
 resource "aws_internet_gateway" "gw" {
     vpc_id = "${aws_vpc.main.id}"
+
+    lifecycle = {
+      create_before_destroy = true
+    }
 }
 
 resource "aws_route_table" "r" {
@@ -20,9 +32,17 @@ resource "aws_route_table" "r" {
         cidr_block = "0.0.0.0/0"
         gateway_id = "${aws_internet_gateway.gw.id}"
     }
+
+    lifecycle = {
+      create_before_destroy = true
+    }
 }
 
 resource "aws_route_table_association" "a" {
     subnet_id = "${aws_subnet.main.id}"
     route_table_id = "${aws_route_table.r.id}"
+
+    lifecycle = {
+      create_before_destroy = true
+    }
 }
